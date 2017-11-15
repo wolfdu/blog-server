e426c40476326246e2fd64d67192dd7c95a5f2ed
Du YueHan <yhdu35@foxmail.com>
2017-11-15 20:36:17 +0800

    what: pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'blog-server',
+      script: './server'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'wolfdu', // 登录远程服务器用户名
+      host: '47.96.160.254', // 服务器ip
+      ref: 'blog-server/master', // 远端名称/分支或主干
+      repo: 'git@github.com:wolfdu/blog-server.git', // 仓库地址
+      path: '/home/wolfdu/project/blog-server',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'env': {
+        'NODE_ENV': 'production'
+      }
+    }
+  }
+}
