88df2b4272c80dd8c9764967da2dd46cb5e24355
wolfdu <yhdu35@foxmail.com>
2017-11-16 11:20:26 +0800

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:wolfdu/blog-server.git',
       path: '/home/wolfdu/project/blog-serve',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 }
